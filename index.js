ace9cf95edcfbcf9f0e3dcdcca60d805594de2e6
Victoria <victoria.fischer@cix.csi.cuny.edu>
2021-01-21 23:47:51 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const { ApolloServer, PubSub } = require('apollo-server')
+
+const mongoose = require('mongoose')
+
+const { MONGODB } = require('./config.js');
+const resolvers = require('./graphql/resolvers')
+const typeDefs = require('./graphql/typeDefs')
+
+const pubsub = new PubSub();
+
+const PORT = process.env.port || 5000;
+
+
+mongoose
+    .connect(MONGODB, 
+        { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('MONGODB Connected')
+        return server.listen({ port: PORT })
+    })
+    .then((res) => {
+        console.log(`Server running at ${res.url}`)
+    })
+    .catch(err => {
+        console.error(err)
+    }) 
+
+//set up Apollo server
+
+const server = new ApolloServer ( {
+    typeDefs,
+    resolvers,
+    context: ({ req }) => ({ req, pubsub })
+})
